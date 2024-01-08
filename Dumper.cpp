b8f524982ec6a54d2417f35006f3717cbdd85e8d
Aashu Krishnan <gpstore31@gmail.com>
2024-01-08 02:34:37 -0600

    Added scrubbing functionality to remove unwanted data from report

--- Dumper.cpp
+++ Dumper.cpp
@@ -7,27 +7,38 @@ int main()
 	std::cin.get();
 	std::cout << std::endl;
 
+	// Dump command output to temporary file to be scrubbed afterwards
 	std::cout << "Dumping general system info..." << std::endl << std::endl;
 
-	system("systeminfo>YourPCSpecs.txt");
+	system("systeminfo>temp.txt");
 
 	std::cout << "Dumping CPU info..." << std::endl << std::endl;
 
-	system("echo: >> YourPCSpecs.txt");
-	system("echo CPU INFO>>YourPCSpecs.txt");
-	system("wmic cpu get caption, deviceid, name, numberofcores, maxclockspeed, status |more>>YourPCSpecs.txt");
+	system("echo: >> temp.txt");
+	system("echo CPU INFO>>temp.txt");
+	system("wmic cpu get caption, deviceid, name, numberofcores, maxclockspeed, status |more>>temp.txt");
 
 	std::cout << "Dumping graphics card info..." << std::endl << std::endl;
 
-	system("echo: >> YourPCSpecs.txt");
-	system("echo GPU INFO>>YourPCSpecs.txt");
-	system("wmic path win32_VideoController get name |more>>YourPCSpecs.txt");
+	system("echo: >> temp.txt");
+	system("echo GPU INFO>>temp.txt");
+	system("wmic path win32_VideoController get name |more>>temp.txt");
 
 	std::cout << "Dumping memory info..." << std::endl << std::endl;
 
-	system("echo: >> YourPCSpecs.txt");
-	system("echo RAM INFO>>YourPCSpecs.txt");
-	system("wmic memorychip get speed |more>>YourPCSpecs.txt");
+	system("echo: >> temp.txt");
+	system("echo RAM INFO>>temp.txt");
+	system("wmic memorychip get speed |more>>temp.txt");
+
+	// Scrub unneeded lines and dump to final file
+	std::cout << "Scrubbing unwanted info..." << std::endl << std::endl;
+
+	system("findstr /V /C:\"Host Name\" /C:\"Registered Owner\" /C:\"Registered Organization\" /C:\"Product ID\" /C:\"Domain\" /C:\"Logon Server\" /C:\"192.168\" /C:\"IP address\" /C:\"Hyper-V\" /C:\"::\" /C:\"Locale\" /C:\"Time Zone\" /C:\"Hotfix\" /C:\"KB\" /C:\"Virtual Memory\" /C:\"OS Manufacturer\" /C:\"OS Configuration\" /C:\"OS Build Type\" temp.txt >YourPCSpecs.txt");
+
+	// Delete temporary file
+	std::cout << "Cleaning up..." << std::endl << std::endl;
+
+	system("del /Q temp.txt");
 
 	// Completion message and waiting for user to exit.
 	std::cout << "Spec dumping complete! You can find your specs in YourPCSpecs.txt, located in the same folder as this exe. Press enter to exit.";
